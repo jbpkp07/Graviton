c8ffdf4f93d80f7d7abaacd082fedc9323138473
jbpkp07 <jeremy.w.barnes@gmail.com>
2019-12-16 18:23:41 -0800

    Hot reloading in place, flushing out better npm scripts.

--- main/src/main.ts
+++ main/src/main.ts
@@ -2,15 +2,21 @@
 // const {app, BrowserWindow} = require('electron')
 // const path = require('path')
 import { app, BrowserWindow, BrowserWindowConstructorOptions } from "electron";
+import electronIsDev from "electron-is-dev";
 import path from "path";
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow: BrowserWindow | null;
+let mainWindow: BrowserWindow | null = null;
 
 
 function createWindow(): void {
     // Create the browser window.
+
+    if (mainWindow !== null) {
+
+        throw new Error("Electron main window already created.");
+    }
 
     const options: BrowserWindowConstructorOptions = {
 
@@ -27,10 +33,27 @@ function createWindow(): void {
     mainWindow = new BrowserWindow(options);
 
     // and load the index.html of the app.
-    mainWindow.loadFile("./index.html");
+    // mainWindow.loadFile("./index.html");
 
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    if (electronIsDev) {
+
+        mainWindow.loadURL("http://localhost:3000")
+
+            .then((): void => {
+                // @ts-ignore
+                mainWindow.webContents.openDevTools(); // Open the DevTools.
+            })
+            .catch((err: string) => {
+
+                throw new Error(err);
+            });
+    }
+    else {
+
+        const prodPath: string = `file://${path.join(__dirname, "./index.html")}`;
+
+        mainWindow.loadURL(prodPath);
+    }
 
     // Emitted when the window is closed.
     mainWindow.on("closed", (): void => {
@@ -41,10 +64,15 @@ function createWindow(): void {
     });
 }
 
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
+
+// On macOS it's common to re-create a window in the app when the
+// dock icon is clicked and there are no other windows open.
+app.on("activate", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", (): void => {
@@ -53,15 +81,6 @@ app.on("window-all-closed", (): void => {
     if (process.platform !== "darwin") {
 
         app.quit();
-    }
-});
-
-app.on("activate", (): void => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-
-        createWindow();
     }
 });
 
