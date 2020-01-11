ccc3576b9603f1dc70297a88a152f02fc03861b8
dspira <daniel.b.spira@gmail.com>
2020-01-10 18:21:24 -0800

    testing push to master

--- renderer/src/screens/DanielDev/DanielDev.tsx
+++ renderer/src/screens/DanielDev/DanielDev.tsx
@@ -1,14 +1,38 @@
 import React from "react";
 
+import { Button, IButtonProps } from "../../components/Button/Button";
 import "./DanielDev.css";
+import { ITextBoxProps, TextBox } from "../../components/TextBox/TextBox";
 
 
 export function DanielDevScreen(): JSX.Element {
 
+    const submitButtonProps: IButtonProps = {
+
+        id: "submitButton",
+        isActive: true,
+        label: "Submit",
+        onClick: (): void => {},
+        positionLeft: "150px",
+        positionTop: "180px"
+    };
+
+    const TextBoxProps: ITextBoxProps = {
+
+        id: "submitButton",
+        isActive: true,
+        onClick: (): void => {},
+        placeholder: "Submit",
+        positionLeft: "150px",
+        positionTop: "220px",
+        width: "100px"
+    };
+
     return (
 
         <div id="danielDevScreen">
-            Go Ravens!
+            <Button {...submitButtonProps} />
+            <TextBox {...TextBoxProps} />
         </div>
     );
 }
